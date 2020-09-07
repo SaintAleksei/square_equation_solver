82b6375d76da807adf03751dc9d9a39281aa81bc
SaintAleksei <70322975+SaintAleksei@users.noreply.github.com>
2020-09-07 14:22:38 +0300

    Update SolveSquare.c
    Сorrected a little

--- SolveSquare.c
+++ SolveSquare.c
@@ -4,8 +4,6 @@
 
 //-----------------------------------------------------------------------------
 
-#define VALID_INPUT 0
-#define INVALID_INPUT 1
 #define INFINITE -1
 #define INFELICITY 1E-6
 
@@ -14,13 +12,12 @@
 int solve_linear (double, double, double*); // solves linear equation
 int solve_square (double, double, double, double*, double*); // solves square equation
 int comparsion (double, double); // compares two real numbers excluding infelicity
-int input (char, double*); // responsible for entering
+int input_double (char, double*); // responsible for entering
 
 //-----------------------------------------------------------------------------
 
 int main()
 {
-
     int r_count = 0, input_stat = INVALID_INPUT;
     double a = 0, b = 0, c = 0, root1 = 0, root2 = 0;
 
@@ -29,9 +26,9 @@ int main()
             "# Enter coefficients from ax^2 - bx + c = 0:\n"
             "#\n"                                           );
 
-    input ('a', &a);
-    input ('b', &b);
-    input ('c', &c);
+    input_double ('a', &a);
+    input_double ('b', &b);
+    input_double ('c', &c);
 
     printf ("# \n");
 
@@ -39,7 +36,6 @@ int main()
 
     switch (r_count)
     {
-
         case 0:        printf ("# Your equation has no roots");
                        break;
 
@@ -54,11 +50,9 @@ int main()
 
         default:       printf ("# Oooops... Something went wrong:\n"
                                "# main(): r_count = %d", r_count);
-
     }
 
     return 0;
-
 }
 
 //-----------------------------------------------------------------------------
@@ -72,26 +66,18 @@ int main()
 
 int comparsion (double a, double b)
 {
-
     if (fabs (a - b) <= INFELICITY)
     {
-
         return 0;
-
     }
     if (a > b)
     {
-
         return 1;
-
     }
     if (a < b)
     {
-
         return -1;
-
     }
-
 }
 
 //-----------------------------------------------------------------------------
@@ -105,24 +91,18 @@ int comparsion (double a, double b)
 
 int solve_linear (double a, double b, double* root_ad)
 {
-
     if (comparsion (a, 0) == 0 && comparsion (b, 0) == 0)
     {
-
         return INFINITE;
-
     }
     if (comparsion (a, 0) == 0 && comparsion (b, 0) != 0)
     {
-
         return 0;
-
     }
 
     *root_ad = -b / a;
 
     return 1;
-
 }
 
 //-----------------------------------------------------------------------------
@@ -136,56 +116,44 @@ int solve_linear (double a, double b, double* root_ad)
 
 int solve_square (double a, double b, double c, double* root1_ad, double* root2_ad)
 {
-
     if (comparsion (a, 0) == 0)
     {
-
         return solve_linear (b, c, root1_ad);
-
     }
 
     double discr = b * b - 4 * a * c;
 
     if (comparsion (discr, 0) == -1)
     {
-
         return 0;
-
     }
     if (comparsion (discr, 0) == 0)
     {
-
         *root1_ad = -b / (2 * a);
 
         return 1;
-
     }
 
     *root1_ad = (-b + sqrt (discr) / (2 * a));
     *root2_ad = (-b - sqrt (discr) / (2 * a));
 
     return 2;
-
 }
 
 //-----------------------------------------------------------------------------
 
-int input (char name, double* value)
+int input_double (char name, double* value)
 {
-
     printf ("# %c = ", name);
 
     if (!scanf ("%lg", value))
     {
-
         printf ("# Invalid input: enter a real numbers:\n");
 
-        return input (name, value);
-
+        return input_double (name, value);
     }
 
     return 0;
-
 }
 
 
